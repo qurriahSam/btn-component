9827418e67d48f16278f748cf3a64d5ce2ab36e9
qurriahSam <mugirethe1@gmail.com>
2023-06-07 21:24:37 +0300

    feat: create nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { NavSectionComponent } from './nav-section/nav-section.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent],
+  declarations: [AppComponent, ButtonComponent, NavSectionComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/nav-section/nav-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-section',
+  templateUrl: './nav-section.component.html',
+  styleUrls: ['./nav-section.component.css']
+})
+export class NavSectionComponent {
+
+}
