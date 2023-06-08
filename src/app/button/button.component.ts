fd1be38e4b9427ca9a859ec72935f8792f5d8085
qurriahSam <mugirethe1@gmail.com>
2023-06-08 23:21:26 +0300

    feat: check changes on input & update button style

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { ButtonTypeStyle } from '../types';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  @Input() buttonType = '';
 
+  buttonTypeStyle: ButtonTypeStyle = 'btn-light';
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['buttonType']) {
+      this.buttonTypeStyle = this.checkBtnType(
+        changes['buttonType'].currentValue
+      );
+    }
+  }
+
+  private checkBtnType(type: string) {
+    switch (type) {
+      case 'outline':
+        return 'btn-outline-primary';
+
+      case 'primary':
+        return 'btn-primary';
+
+      default:
+        return 'btn-light';
+    }
+  }
 }
