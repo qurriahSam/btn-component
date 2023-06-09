34d37e6012846b2c46ebc2d45835254b64fbc708
qurriahSam <mugirethe1@gmail.com>
2023-06-09 19:47:56 +0300

    feat: add button variables and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Icon } from './types';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  outline = 'outline';
-  primary = 'primary';
-  default = '';
+  button = ['outline', 'primary', ''];
+  icons: Icon[] = [
+    'bi-brightness-high',
+    'bi-alarm',
+    'bi-browser-chrome',
+    'bi-broadcast-pin',
+    'bi-reddit',
+    '',
+  ];
 }
