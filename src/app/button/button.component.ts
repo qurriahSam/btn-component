e1f1bfa0c6d7c6b4241534c06539a2b1f2e62b11
qurriahSam <mugirethe1@gmail.com>
2023-06-09 19:53:51 +0300

    feat: add props to button component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
-import { ButtonTypeStyle } from '../types';
+import { ButtonTypeStyle, Icon } from '../types';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
@@ -7,9 +7,12 @@ import { ButtonTypeStyle } from '../types';
 })
 export class ButtonComponent {
   @Input() buttonType = '';
+  @Input() shadow = true;
+  @Input() disable = false;
+  @Input() icon: Icon = '';
+  @Input() right = false;
 
   buttonTypeStyle: ButtonTypeStyle = 'btn-light';
-
   ngOnChanges(changes: SimpleChanges) {
     if (changes['buttonType']) {
       this.buttonTypeStyle = this.checkBtnType(
