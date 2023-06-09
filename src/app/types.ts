f5c758887c1ffeb5b36786fc3d30415ee39e6b26
qurriahSam <mugirethe1@gmail.com>
2023-06-09 19:48:20 +0300

    feat: create icon types

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,3 +2,11 @@ export type ButtonTypeStyle =
   | 'btn-outline-primary'
   | 'btn-primary'
   | 'btn-light';
+
+export type Icon =
+  | 'bi-brightness-high'
+  | 'bi-alarm'
+  | 'bi-browser-chrome'
+  | 'bi-broadcast-pin'
+  | 'bi-reddit'
+  | '';
